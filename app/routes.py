05de2c59fc9ed42d80c30e980d1d21b6b90c07fb
Ibsy Matute <ibsy@rocketmail.com>
2022-08-15 22:29:53 -0500

    added cards with an image correlating with the film title to the faves page

--- app/routes.py
+++ app/routes.py
@@ -9,9 +9,9 @@ def index():
 
 @app.route('/faves')
 def test():
-    fav_ghibli = ['Princess Mononoke', 
-    'Nausicaa of the Valley of the Wind',
-    'Kiki\'s Delivery Service',
-    'Spirited Away',
-    'Ponyo']
+    fav_ghibli = {'Princess Mononoke': 'https://www.ghibli.jp/gallery/mononoke015.jpg',
+    'Nausicaa of the Valley of the Wind': 'https://www.ghibli.jp/gallery/nausicaa044.jpg',
+    'Kiki\'s Delivery Service': 'https://www.ghibli.jp/gallery/majo024.jpg',
+    'Spirited Away': 'https://www.ghibli.jp/gallery/chihiro045.jpg',
+    'Ponyo': 'https://www.ghibli.jp/gallery/ponyo028.jpg'}
     return render_template('faves.html', fav_ghibli = fav_ghibli)
